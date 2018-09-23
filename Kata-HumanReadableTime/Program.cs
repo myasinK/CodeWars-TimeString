9bc766e891f4812a059070102ca04c050140bade
myasinK <yasinul@gmail.com>
2018-09-23 14:21:25 -0700

    Kata-Int to to timestring

---
+++ Kata-HumanReadableTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    //Write a function, which takes a non-negative integer(seconds) as input and returns the time in a human-readable format(HH:MM:SS)
+
+    //HH = hours, padded to 2 digits, range: 00 - 99
+    //MM = minutes, padded to 2 digits, range: 00 - 59
+    //SS = seconds, padded to 2 digits, range: 00 - 59
+    //The maximum time never exceeds 359999 (99:59:59)
+
+    //You can find some examples in the test fixtures.
+
+namespace Kata_HumanReadableTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = 0;
+            var timeString = TimeFormat.GetReadableTime(t);
+            Console.WriteLine(timeString);
+        }
+    }
+}
---
+++ Kata-HumanReadableTime/TimeFormat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Kata_HumanReadableTime
+{
+    public static class TimeFormat
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            if (seconds == 0)
+            {
+                return "00:00:00";
+            }
+            else if (seconds >= 359999)
+            {
+                return "99:59:59";
+            }
+            else
+            {
+                var secondsInHr = 60 * 60;
+                var secondsInMin = 60;
+                var HH = ( seconds / secondsInHr ).ToString("D2");
+                var MM = ( (seconds % secondsInHr) / secondsInMin ).ToString("D2");
+                var SS = ( (seconds % secondsInHr) % secondsInMin ).ToString("D2");
+                return $"{HH}:{MM}:{SS}";
+
+            }
+
+        }
+    }
+}
